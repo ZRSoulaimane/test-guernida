404bb20cfa7e09e7b634b1a60d3005a4494f38a9
ZRSoulaimane <szarria@gear9.ma>
2023-05-31 21:42:11 +0100

    refactor - api

--- execution/linkedin_crawling.py
+++ execution/linkedin_crawling.py
@@ -32,6 +32,7 @@ def get_chromedriver():
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument('--headless')
 
     return webdriver.Chrome(options=chrome_options)
 
